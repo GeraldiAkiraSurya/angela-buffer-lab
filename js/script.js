69793e4581fffeb257b8f7291f91c65481ea9c50
Geraldi Akira Surya <geraldiakira@gmail.com>
2023-04-13 22:37:50 +0700

    Progress sampe misi 1-1

--- js/script.js
+++ js/script.js
@@ -9,7 +9,50 @@ monogatari.action ('message').messages ({
 			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
 			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
 		`
-	}
+	},
+	'Narration1-1-1': {
+		title: '',
+		subtitle: '',
+		body: 'Allison adalah seorang profesor yang ditugaskan di Laboratorium X untuk mencari obat dari penyakit yang tidak diketahui asal-usulnya.',
+	},
+	'Narration1-1-2': {
+		title: '',
+		subtitle: '',
+		body: 'Untuk itu Prof. Allison bekerjasama dengan ilmuan kimia (pemain) untuk melakukan penelitian untuk mengumpulkan ramuan obat yang sedang dicari.',
+	},
+	'Narration1-1-3': {
+		title: '',
+		subtitle: '',
+		body: 'Seluruh ramuan obat dapat dikumpulkan setelah ilmuan kimia menyelesaikan tantangan pada setiap misi.',
+	},
+	'NarrMiss1-1-1': {
+		title: 'Misi 1',
+		subtitle: '',
+		body: `
+			<p><b>SYARAT PENYELESAIAN KHUSUS:</b> AMATILAH VIDEO DENGAN SEKSAMA DAN PILIHLAH JAWABAN YANG BENAR DARI PERTANYAAN YANG DIBERIKAN</p>
+			<p><b>BATAS WAKTU:</b> TIDAK ADA</p>
+			<p><b>HADIAH:</b> ENERGI DAN BUKU RAMUAN OBAT</p>
+			<p><b>JIKA GAGAL:</b> KEMBALI KE AWAL</p>
+		`,
+	},
+	'NarrMiss1-1-2': {
+		title: 'Video 1',
+		subtitle: '',
+		body: `
+			<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/tgbNymZ7vqY" allowFullScreen="true">
+			</iframe>
+		`
+	},
+	'NarrMiss1-1-3': {
+		title: 'Video 2',
+		subtitle: '',
+		body: `
+			<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/tgbNymZ7vqY" allowFullScreen="true">
+			</iframe>
+		`
+	},
 });
 
 // Define the notifications used in the game
@@ -69,26 +112,39 @@ monogatari.assets ('images', {
 
 // Define the backgrounds for each scene.
 monogatari.assets ('scenes', {
-
+	'lab1': 'science_lab.jpg',
+	'desk': 'lab_desk.jpg'
 });
 
 
 // Define the Characters
 monogatari.characters ({
-	'y': {
-		name: 'Yui',
-		color: '#5bcaff'
+	'p': {
+		name: 'Prof. Allison',
+		color: '#5bcaff',
+		sprites: {
+			normal: 'professor.png'
+		}
+	},
+	'u': {
+		name: '{{player.name}}',
+		color: '#FFFFFF'
 	}
 });
 
 monogatari.script ({
 	// The game starts here.
 	'Start': [
-		'show scene #f7f6f6 with fadeIn',
+		'show scene lab1 with fadeIn',
+		'show message Narration1-1-1',
+		'show message Narration1-1-2',
+		'show message Narration1-1-3',
+		'show character p normal with fadeIn',
+		'p Hai selamat datang di Buffer Laboratory!!!',
 		'show notification Welcome',
 		{
 			'Input': {
-				'Text': 'What is your name?',
+				'Text': 'Siapa namamu?',
 				'Validation': function (input) {
 					return input.trim ().length > 0;
 				},
@@ -107,40 +163,58 @@ monogatari.script ({
 						}
 					});
 				},
-				'Warning': 'You must enter a name!'
+				'Peringatan': 'Kamu harus menuliskan namamu!'
 			}
 		},
-		'y Hi {{player.name}} Welcome to Monogatari!',
+		'p Hi {{player.name}} perkenalkan aku adalah professor Allison!',
+		'p {{player.name}}… Anda harus mengambil buku ramuan obat yang tertinggal di laboratorium pusat!',
+		'u Siap meluncur prof...',
+		'p Sebelum berangkat Anda harus menyelesaikan tantangan di laboratorium X, tiap tantangan yang dilewati akan menambah energi Anda untuk memperoleh buku ramuan obat..',
+		'u Penemuan ini akan menjadi gebrakan hebat di dunia penelitian…',
+		'p Semoga berhasil!!!',
+		'Anda dapat memulai penelitian di laboratorium X',
 		{
 			'Choice': {
-				'Dialog': 'y Have you already read some documentation?',
+				// 'Dialog': 'Anda dapat memulai penelitian di laboratorium X',
 				'Yes': {
-					'Text': 'Yes',
-					'Do': 'jump Yes'
+					'Text': 'Terima',
+					'Do': 'jump Misi1-1'
 				},
-				'No': {
-					'Text': 'No',
+				'Tidak': {
+					'Text': 'Tidak',
 					'Do': 'jump No'
 				}
 			}
-		}
+		},
 	],
 
-	'Yes': [
-		'y Thats awesome!',
-		'y Then you are ready to go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
+	'Misi1-1': [
+		'show scene desk with FadeIn',
+		'show message NarrMiss1-1-1',
+		'show message NarrMiss1-1-2',
+		'show message NarrMiss1-1-3',
+		'Berdasarkan tayangan video tersebut, coba bandingkan perubahan pH pada sistem 1 sampai 4 sebelum dan sesudah ditambah sedikit asam maupun basa. Manakah pernyataan yang benar?',
+		{
+			'Choice': {
+				'No': {
+					'Text': 'pH pada semua system/ campuran berubah drastis',
+					'Do': 'jump No'
+				},
+				'Yes': {
+					'Text': 'pH pada Campuran 15 mL CH3COOH 0,1 M + 15 mL CH3COONa 15 M tidak berubah secara signifikan',
+					'Do': 'jump Misi1-2'
+				}
+			}
+		},
+		
 		'end'
 	],
 
 	'No': [
-
-		'y You can do it now.',
-
-		'show message Help',
-
-		'y Go ahead and create an amazing Game!',
-		'y I can’t wait to see what story you’ll tell!',
-		'end'
+		// 'y You can do it now.',
+		// 'show message Help',
+		// 'y Go ahead and create an amazing Game!',
+		// 'y I can’t wait to see what story you’ll tell!',
+		// 'end'
 	]
 });
