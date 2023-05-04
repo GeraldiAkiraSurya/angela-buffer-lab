f5f715460e88112ecc8a83adfd21be68e8b2ecf1
Lucyus Matthew Ardivan <6181801029@student.unpar.ac.id>
2023-05-04 23:19:16 +0700

    adding canvas width and height

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -14,14 +14,18 @@ mainMenu.create = function () {
     // console.log(game.canvas.width, game.canvas.height);
     //x 1879 y 1008
 
+    canvasWidth = game.canvas.width;
+    canvasHeight = game.canvas.height;
+    //git error
+
     //background main menu
-    this.add.image(game.canvas.width/2, game.canvas.height/2, 'menuBackground').setScale(0.65, 0.52);
+    this.add.image(canvasWidth/2, canvasHeight/2, 'menuBackground').setScale(0.65, 0.52);
 
     energy = 100;
 
     //buttons
-    btnStart = this.add.image(game.canvas.width/2, game.canvas.height/2, 'startButton').setInteractive().setScale(0.5);
-    btnOptions = this.add.image(game.canvas.width/2, game.canvas.height/2 + 125, 'optionsButton').setInteractive().setScale(0.5);
+    btnStart = this.add.image(canvasWidth/2, canvasHeight/2, 'startButton').setInteractive().setScale(0.5);
+    btnOptions = this.add.image(canvasWidth/2, canvasHeight/2 + 125, 'optionsButton').setInteractive().setScale(0.5);
 
     btnStart.on('pointerup', function () {        
         console.log(energy);
@@ -35,7 +39,7 @@ mainMenu.create = function () {
     // this.tweens.add({
     //     targets: [btnStart],
     //     duration: 1000,
-    //     x: game.canvas.width/2,
+    //     x: canvasWidth/2,
     //     scaleX: 2,
     //     scaleY: 2,
     //     ease: 'Bounce.easeIn'
