7a06e1a7e5206b7d641deb2d03e1a4dcc75275e2
Lucyus Matthew Ardivan <6181801029@student.unpar.ac.id>
2023-05-22 16:03:26 +0700

    2.1.9 menunggu croppan jawaban

--- js/question219.js
+++ js/question219.js
@@ -2,8 +2,8 @@ question219 = new Phaser.Scene('Question219');
 
 question219.preload = function () {
     this.load.path = './assets/';
-    this.load.image('emptyBeaker', 'question/emptyBeaker.png');
-    this.load.image('purpleBeaker', 'question/purpleBeaker.png');
+    this.load.image('beaker', 'question/beaker.gif');
+    this.load.image('purpleBeaker', 'question/bottle.png');
 
     this.load.image('219A', 'question/2.1.1A.png');
     this.load.image('219B', 'question/2.1.1B.png');
@@ -16,12 +16,11 @@ question219.preload = function () {
     this.load.plugin('rexbbcodetextplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexbbcodetextplugin.min.js', true);
 }
 
-var beakerJawaban1;
-var beakerJawaban2;
-var beakerJawaban3;
-var beakerJawaban4;
-var beakerJawaban5;
-var dropZoneEmptyBeakerBG;
+var botolJawaban1;
+var botolJawaban2;
+var botolJawaban3;
+var botolJawaban4;
+var dropZoneBeakerBG;
 
 var textPertanyaan;
 
@@ -57,31 +56,27 @@ question219.create = function () {
     objectsArray = [];
 
     //beaker jawaban
-    beakerJawaban1 = this.add.image(middleX + 230, middleY - 370, 'purpleBeaker').setInteractive().setScale(0.19).setName('A');
-    this.input.setDraggable(beakerJawaban1);
-    objectsArray.push(beakerJawaban1);
+    botolJawaban1 = this.add.image(middleX - 30, middleY - 20, 'purpleBeaker').setInteractive().setScale(0.25).setName('A');
+    this.input.setDraggable(botolJawaban1);
+    objectsArray.push(botolJawaban1);
 
-    beakerJawaban2 = this.add.image(middleX + 830, middleY - 370, 'purpleBeaker').setInteractive().setScale(0.19).setName('B');
-    this.input.setDraggable(beakerJawaban2);
-    objectsArray.push(beakerJawaban2);
+    botolJawaban2 = this.add.image(middleX + 240, middleY - 160, 'purpleBeaker').setInteractive().setScale(0.25).setName('B');
+    this.input.setDraggable(botolJawaban2);
+    objectsArray.push(botolJawaban2);
 
-    beakerJawaban3 = this.add.image(middleX + 530, middleY - 130, 'purpleBeaker').setInteractive().setScale(0.19).setName('C');
-    this.input.setDraggable(beakerJawaban3);
-    objectsArray.push(beakerJawaban3);
+    botolJawaban3 = this.add.image(middleX + 540, middleY - 20, 'purpleBeaker').setInteractive().setScale(0.25).setName('C');
+    this.input.setDraggable(botolJawaban3);
+    objectsArray.push(botolJawaban3);
 
-    beakerJawaban4 = this.add.image(middleX + 230, middleY + 110, 'purpleBeaker').setInteractive().setScale(0.19).setName('D');
-    this.input.setDraggable(beakerJawaban4);
-    objectsArray.push(beakerJawaban4);
-
-    beakerJawaban5 = this.add.image(middleX + 830, middleY + 110, 'purpleBeaker').setInteractive().setScale(0.19).setName('E');
-    this.input.setDraggable(beakerJawaban5);
-    objectsArray.push(beakerJawaban5);
+    botolJawaban4 = this.add.image(middleX + 810, middleY - 160, 'purpleBeaker').setInteractive().setScale(0.25).setName('D');
+    this.input.setDraggable(botolJawaban4);
+    objectsArray.push(botolJawaban4);
 
 
     //beaker drop zone
-    dropZoneEmptyBeakerBG = this.add.image(middleX - 500, middleY - 10, 'emptyBeaker').setScale(0.4);
+    dropZoneBeakerBG = this.add.image(middleX - 540, middleY - 20, 'beaker').setScale(1.1);
 
-    const answerZone = this.add.zone(middleX - 500, middleY - 10, dropZoneEmptyBeakerBG.width * 0.4, dropZoneEmptyBeakerBG.height * 0.4).setRectangleDropZone(dropZoneEmptyBeakerBG.width * 0.4, dropZoneEmptyBeakerBG.height  * 0.4).setName('answer');
+    const answerZone = this.add.zone(middleX - 540, middleY - 20, dropZoneBeakerBG.width * 1.1, dropZoneBeakerBG.height * 1.1).setRectangleDropZone(dropZoneBeakerBG.width * 1.1, dropZoneBeakerBG.height  * 1.1).setName('answer');
 
     //just a visual display of the drop zone
     // const graphics = this.add.graphics();
@@ -89,12 +84,11 @@ question219.create = function () {
     // graphics.strokeRect(answerZone.x - answerZone.input.hitArea.width / 2, answerZone.y - answerZone.input.hitArea.height / 2, answerZone.input.hitArea.width, answerZone.input.hitArea.height);
 
     //text pertanyaan
-    textPertanyaan = this.add.text(middleX - 870, middleY - 454, "Tuangkan isi\ngelas yang benar\nke gelas kosong", {font: "900 50px Helvetica", fill: "#ffffff"});
+    textPertanyaan = this.add.text(middleX - 870, middleY - 454, "Tuangkan isi\nbotol yang benar\nke gelas kimia..", {font: "900 50px Helvetica", fill: "#ffffff"});
 
     var text = `
-Pilihlah reaksi disosiasi yang terjadi
-
-pada masing - masing larutan?`;
+Nah..berdasarkan perubahan pH pada campuran / sistem dalam tabel, manakah yang termasuk larutan penyangga?
+`;
 
     //beaker-text
     A = this.add.image(middleX + 30, middleY - 300, '219A').setScale(1.2);
@@ -112,10 +106,6 @@ pada masing - masing larutan?`;
     D = this.add.image(middleX + 30, middleY + 180, '219D').setScale(1.2);
     // this.input.setDraggable(D);
     objectsArray.push(D);
-
-    E = this.add.image(middleX + 630, middleY + 180, '219E').setScale(1.2);
-    // this.input.setDraggable(E);
-    objectsArray.push(E);
 
     this.input.on('pointerdown', () => {
     });
@@ -176,10 +166,10 @@ pada masing - masing larutan?`;
 
     //alur game
     //hide all object
-    hideObject(objectsArray);
+    // hideObject(objectsArray);
 
     //show pertanyaan
-    showPertanyaan219(this, text);
+    // showPertanyaan219(this, text);
 }
 
 question219.update = function () {
@@ -188,10 +178,17 @@ question219.update = function () {
 
 function showPertanyaan219(scene, text) {
     var descriptionBox = scene.add.rectangle(scene.cameras.main.width / 2, scene.cameras.main.height / 2, scene.cameras.main.width / 2, scene.cameras.main.height * 3 / 4, 0x000000, 0.7);
+    var descBoxTopX = middleX - (descriptionBox.width / 2);
+    var descBoxTopY = middleY - (descriptionBox.height / 2);
 
-    let missionDesc = scene.add.text(middleX, middleY, text)
-        .setOrigin(0.5)
-        .setFontSize('30px');
+    let missionDesc = scene.add.rexBBCodeText(middleX-descBoxTopX+50, descBoxTopY+250, text, {
+        fontSize: '30px',
+        align: 'left',
+        wrap: {
+            mode: 'word',
+            width: descriptionBox.width-100
+        },
+    });
 
     var nextBtn = createNextButton(scene, 'LANJUT', () => {
         missionDesc.destroy();
@@ -296,20 +293,17 @@ function startOver219(scene) {
     //kosongin jawaban buat startover
     playerAnswerArray = [];
 
-    beakerJawaban1 = scene.add.image(middleX + 230, middleY - 370, 'purpleBeaker').setInteractive().setScale(0.19).setName('A');
-    scene.input.setDraggable(beakerJawaban1);
+    botolJawaban1 = scene.add.image(middleX + 230, middleY - 370, 'purpleBeaker').setInteractive().setScale(0.19).setName('A');
+    scene.input.setDraggable(botolJawaban1);
 
-    beakerJawaban2 = scene.add.image(middleX + 830, middleY - 370, 'purpleBeaker').setInteractive().setScale(0.19).setName('B');
-    scene.input.setDraggable(beakerJawaban2);
+    botolJawaban2 = scene.add.image(middleX + 830, middleY - 370, 'purpleBeaker').setInteractive().setScale(0.19).setName('B');
+    scene.input.setDraggable(botolJawaban2);
 
-    beakerJawaban3 = scene.add.image(middleX + 530, middleY - 130, 'purpleBeaker').setInteractive().setScale(0.19).setName('C');
-    scene.input.setDraggable(beakerJawaban3);
+    botolJawaban3 = scene.add.image(middleX + 530, middleY - 130, 'purpleBeaker').setInteractive().setScale(0.19).setName('C');
+    scene.input.setDraggable(botolJawaban3);
 
-    beakerJawaban4 = scene.add.image(middleX + 230, middleY + 110, 'purpleBeaker').setInteractive().setScale(0.19).setName('D');
-    scene.input.setDraggable(beakerJawaban4);
-
-    beakerJawaban5 = scene.add.image(middleX + 830, middleY + 110, 'purpleBeaker').setInteractive().setScale(0.19).setName('E');
-    scene.input.setDraggable(beakerJawaban5);
+    botolJawaban4 = scene.add.image(middleX + 230, middleY + 110, 'purpleBeaker').setInteractive().setScale(0.19).setName('D');
+    scene.input.setDraggable(botolJawaban4);
 
     A = scene.add.image(middleX + 30, middleY - 300, '219A').setScale(1.2);
 
@@ -318,8 +312,6 @@ function startOver219(scene) {
     C = scene.add.image(middleX + 330, middleY - 60, '219C').setScale(1.2);
 
     D = scene.add.image(middleX + 30, middleY + 180, '219D').setScale(1.2);
-
-    E = scene.add.image(middleX + 630, middleY + 180, '219E').setScale(1.2);
 
     //btn buat check jawaban kedua kali
     cekJawabanBtn = createNextButton(scene, 'CEK JAWABAN', () => {
