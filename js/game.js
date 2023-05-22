199e18f3311a5cc6c5acb4abed576e629102cd93
Lucyus Matthew Ardivan <6181801029@student.unpar.ac.id>
2023-05-22 13:09:40 +0700

    coba bikin pertanyaan 1

--- js/game.js
+++ js/game.js
@@ -48,8 +48,8 @@ var config = {
     // scene: [mainMenu, findingObjects1, findingObjects2, question212, selectChara, missionSelection, missionOne, missionThree]
 
     //development
-    scene: [question214]
-    // scene: [question212, question214]
+    // scene: [question214]
+    scene: [question212, question214]
     // scene: [missionOne]
     // scene: [findingObjects1]
 };
