e052e3fdaeac4196efe4da139aaeef82a9e2bdec
Lucyus Matthew Ardivan <6181801029@student.unpar.ac.id>
2023-05-28 17:15:48 +0700

    minor fix pengantar split to 3

--- js/pengantar.js
+++ js/pengantar.js
@@ -5,8 +5,16 @@ pengantar.preload = function () {
     this.load.image('menuBackground', 'scenes/science_lab.jpg');
     this.load.image('nextButton', 'buttons/Continue Button.png');
 
-    this.load.image('infografis', 'lab_eq/Infografis Larutan Penyangga dalam Kehidupan.png');
+    this.load.image('infografis1', 'lab_eq/Infografis Larutan Penyangga dalam Kehidupan1.jpg');
+    this.load.image('infografis2', 'lab_eq/Infografis Larutan Penyangga dalam Kehidupan2.jpg');
+    this.load.image('infografis3', 'lab_eq/Infografis Larutan Penyangga dalam Kehidupan3.jpg');
 }
+
+var infografis1;
+var infografis2;
+var infografis3;
+
+var infografisState;
 
 var btnNext;
 
@@ -21,13 +29,36 @@ pengantar.create = function () {
     this.add.image(middleX, middleY, 'menuBackground').setScale(0.65, 0.52);
 
     //infografis
-    this.add.image(middleX, middleY + 70, 'infografis').setScale(0.6);
+    infografis1 = this.add.image(middleX, middleY - 120, 'infografis1').setScale(1.4);
+    infografis2 = this.add.image(middleX, middleY - 120, 'infografis2').setScale(1.6).setVisible(false);
+    infografis3 = this.add.image(middleX, middleY - 120, 'infografis3').setScale(1.6).setVisible(false);
+
+    infografisState = 0;
 
     //buttons
-    btnNext = this.add.image(middleX + 700, middleY + 400, 'nextButton').setInteractive().setScale(0.5);
+    btnNext = this.add.image(middleX, middleY + 330, 'nextButton').setInteractive().setScale(0.5);
 
     btnNext.on('pointerup', function () {
-        pengantar.scene.start('MissionSelection');
+        if (infografisState == 0) {
+            console.log('maju gambar 2');
+            infografis1.setVisible(false);
+            infografis2.setVisible(true);
+            infografisState++;
+        }
+
+        else if (infografisState == 1) {
+            console.log('maju gambar 3');
+            infografis2.setVisible(false);
+            infografis3.setVisible(true);
+            infografisState++;
+        }
+
+        else if (infografisState == 2) {
+            console.log('started scene MissionSelection');
+            pengantar.scene.start('MissionSelection');
+        }
+
+        // console.log(infografis1.visible);
     });
 }
 
